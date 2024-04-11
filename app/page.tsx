70a44add7e5715f3940855b83477edfb1299038c
alx-yng <alexyoung0119@gmail.com>
2024-04-11 17:10:45 -0500

    fix centering

--- app/page.tsx
+++ app/page.tsx
@@ -9,41 +9,34 @@ export default function Home() {
         <motion.div
           initial={{ top: "48%", left: "45.5%" }}
           animate={{
-            top: 15,
-            left: 15,
+            top: -20,
+            left: 50,
           }}
           transition={{ duration: 2, delay: 1.5, ease: "easeInOut" }}
-          className='absolute flex gap-2 items-center'>
+          className='relative flex gap-2 items-center w-fit bg-black px-4 z-50'>
           <motion.h1
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 2, delay: 0.2 }}
             className='text-3xl'>
-            Alex
+            alex
           </motion.h1>
           <motion.h1
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 2, delay: 0.8 }}
             className='text-3xl'>
-            Young
+            young
           </motion.h1>
         </motion.div>
         <motion.h1
           initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          animate={{ opacity: 1, y: -50 }}
           transition={{ duration: 2, delay: 3.4 }}
-          drag
-          dragConstraints={{
-            top: -50,
-            left: -50,
-            right: 50,
-            bottom: 50,
-          }}
-          dragSnapToOrigin
-          className='text-2xl text-center'>
+          className='text-2xl text-center h-full grid place-items-center'>
           <em>coming soon . . .</em>
         </motion.h1>
+        <div className='bg-black absolute top-[45px] left-[120px] h-4 w-48' />
       </section>
     </main>
   );
