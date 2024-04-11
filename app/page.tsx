1a84146d3b065f8e4cddbfceb19ba9994f85c0c9
alx-yng <alexyoung0119@gmail.com>
2024-04-11 17:18:58 -0500

    fix black bg on name

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,25 @@ export default function Home() {
   return (
     <main className='h-screen overflow-hidden grid place-items-center'>
       <section className='h-[90%] w-[90%] border m-auto'>
+        <div className='bg-black absolute top-[45px] left-[104px] h-4 w-48' />
         <motion.div
           initial={{ top: "48%", left: "45.5%" }}
           animate={{
             top: -20,
             left: 50,
           }}
-          transition={{ duration: 2, delay: 1.5, ease: "easeInOut" }}
-          className='relative flex gap-2 items-center w-fit bg-black px-4 z-50'>
+          transition={{ duration: 2, delay: 2.8, ease: "easeInOut" }}
+          className='relative flex gap-2 items-center w-fit z-50'>
           <motion.h1
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
+            initial={{ opacity: 0, y: -40 }}
+            animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 2, delay: 0.2 }}
             className='text-3xl'>
             alex
           </motion.h1>
           <motion.h1
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 2, delay: 0.8 }}
             className='text-3xl'>
             young
@@ -36,7 +37,6 @@ export default function Home() {
           className='text-2xl text-center h-full grid place-items-center'>
           <em>coming soon . . .</em>
         </motion.h1>
-        <div className='bg-black absolute top-[45px] left-[120px] h-4 w-48' />
       </section>
     </main>
   );
